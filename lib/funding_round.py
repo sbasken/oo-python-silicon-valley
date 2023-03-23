21c1f507b03b6a8e6a0274431004eac2e34ac53b
Saki <sbasken@gmail.com>
2023-03-22 21:52:31 -0700

    instantiate class FundingRound

--- lib/funding_round.py
+++ lib/funding_round.py
@@ -1,2 +1,7 @@
 class FundingRound:
-    pass
+
+    def __init__(self, startup, venture_capitalist, type, investment)
+        self.startup = startup
+        self.venture_capitalist = venture_capitalist
+        self.type = type
+        self.investment = investment
